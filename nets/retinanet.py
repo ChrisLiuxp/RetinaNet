2a4f512b338561caa84c4c9b26588e5114507064
LiuXinpeng2019102939 <136125471@qq.com>
2020-11-09 15:31:18 +0800

    加入BiFPN的网络模型

--- nets/retinanet.py
+++ nets/retinanet.py
@@ -5,6 +5,317 @@ import math
 from nets.resnet import resnet18,resnet34,resnet50,resnet101,resnet152
 from utils.anchors import Anchors
 from nets.head import FCOSClsCenterHead, FCOSRegHead, FCOSPositions
+from nets.layers import MemoryEfficientSwish, Swish
+from nets.layers import Conv2dStaticSamePadding, MaxPool2dStaticSamePadding
+
+# ----------------------------------#
+#   Xception中深度可分离卷积
+#   先3x3的深度可分离卷积
+#   再1x1的普通卷积
+# ----------------------------------#
+class SeparableConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels=None, norm=True, activation=False, onnx_export=False):
+        super(SeparableConvBlock, self).__init__()
+        if out_channels is None:
+            out_channels = in_channels
+
+        self.depthwise_conv = Conv2dStaticSamePadding(in_channels, in_channels,
+                                                      kernel_size=3, stride=1, groups=in_channels, bias=False)
+        self.pointwise_conv = Conv2dStaticSamePadding(in_channels, out_channels, kernel_size=1, stride=1)
+
+        self.norm = norm
+        if self.norm:
+            self.bn = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+
+        self.activation = activation
+        if self.activation:
+            self.swish = MemoryEfficientSwish() if not onnx_export else Swish()
+
+    def forward(self, x):
+        x = self.depthwise_conv(x)
+        x = self.pointwise_conv(x)
+
+        if self.norm:
+            x = self.bn(x)
+
+        if self.activation:
+            x = self.swish(x)
+
+        return x
+
+
+class BiFPN(nn.Module):
+    def __init__(self, num_channels, conv_channels, first_time=False, epsilon=1e-4, onnx_export=False, attention=True):
+        super(BiFPN, self).__init__()
+        self.epsilon = epsilon
+        self.conv6_up = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv5_up = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv4_up = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv3_up = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+
+        self.conv4_down = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv5_down = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv6_down = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+        self.conv7_down = SeparableConvBlock(num_channels, onnx_export=onnx_export)
+
+        self.p6_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p5_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p4_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p3_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+
+        self.p4_downsample = MaxPool2dStaticSamePadding(3, 2)
+        self.p5_downsample = MaxPool2dStaticSamePadding(3, 2)
+        self.p6_downsample = MaxPool2dStaticSamePadding(3, 2)
+        self.p7_downsample = MaxPool2dStaticSamePadding(3, 2)
+
+        self.swish = MemoryEfficientSwish() if not onnx_export else Swish()
+
+        self.first_time = first_time
+        if self.first_time:
+            # 获取到了efficientnet的最后三层，对其进行通道的下压缩
+            self.p5_down_channel = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[2], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+            )
+            self.p4_down_channel = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[1], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+            )
+            self.p3_down_channel = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[0], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+            )
+
+            # 对输入进来的p5进行宽高的下采样
+            self.p5_to_p6 = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[2], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+                MaxPool2dStaticSamePadding(3, 2)
+            )
+            self.p6_to_p7 = nn.Sequential(
+                MaxPool2dStaticSamePadding(3, 2)
+            )
+
+            # BIFPN第一轮的时候，跳线那里并不是同一个in
+            self.p4_down_channel_2 = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[1], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+            )
+            self.p5_down_channel_2 = nn.Sequential(
+                Conv2dStaticSamePadding(conv_channels[2], num_channels, 1),
+                nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
+            )
+
+        # 简易注意力机制的weights
+        self.p6_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.p6_w1_relu = nn.ReLU()
+        self.p5_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.p5_w1_relu = nn.ReLU()
+        self.p4_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.p4_w1_relu = nn.ReLU()
+        self.p3_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.p3_w1_relu = nn.ReLU()
+
+        self.p4_w2 = nn.Parameter(torch.ones(3, dtype=torch.float32), requires_grad=True)
+        self.p4_w2_relu = nn.ReLU()
+        self.p5_w2 = nn.Parameter(torch.ones(3, dtype=torch.float32), requires_grad=True)
+        self.p5_w2_relu = nn.ReLU()
+        self.p6_w2 = nn.Parameter(torch.ones(3, dtype=torch.float32), requires_grad=True)
+        self.p6_w2_relu = nn.ReLU()
+        self.p7_w2 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.p7_w2_relu = nn.ReLU()
+
+        self.attention = attention
+
+    def forward(self, inputs):
+        """ bifpn模块结构示意图
+            P7_0 -------------------------> P7_2 -------->
+               |-------------|                ↑
+                             ↓                |
+            P6_0 ---------> P6_1 ---------> P6_2 -------->
+               |-------------|--------------↑ ↑
+                             ↓                |
+            P5_0 ---------> P5_1 ---------> P5_2 -------->
+               |-------------|--------------↑ ↑
+                             ↓                |
+            P4_0 ---------> P4_1 ---------> P4_2 -------->
+               |-------------|--------------↑ ↑
+                             |--------------↓ |
+            P3_0 -------------------------> P3_2 -------->
+        """
+        if self.attention:
+            p3_out, p4_out, p5_out, p6_out, p7_out = self._forward_fast_attention(inputs)
+        else:
+            p3_out, p4_out, p5_out, p6_out, p7_out = self._forward(inputs)
+
+        return p3_out, p4_out, p5_out, p6_out, p7_out
+
+    def _forward_fast_attention(self, inputs):
+        # 当phi=1、2、3、4、5的时候使用_forward_fast_attention
+        if self.first_time:
+            # 第一次BIFPN需要下采样与降通道获得
+            # p3_in p4_in p5_in p6_in p7_in
+            p3, p4, p5 = inputs
+            p3_in = self.p3_down_channel(p3)
+
+            p4_in_1 = self.p4_down_channel(p4)
+            p4_in_2 = self.p4_down_channel_2(p4)
+
+            p5_in_1 = self.p5_down_channel(p5)
+            p5_in_2 = self.p5_down_channel_2(p5)
+
+            p6_in = self.p5_to_p6(p5)
+            p7_in = self.p6_to_p7(p6_in)
+
+            # 简单的注意力机制，用于确定更关注p7_in还是p6_in
+            p6_w1 = self.p6_w1_relu(self.p6_w1)
+            weight = p6_w1 / (torch.sum(p6_w1, dim=0) + self.epsilon)
+            p6_td = self.conv6_up(self.swish(weight[0] * p6_in + weight[1] * self.p6_upsample(p7_in)))
+
+            # 简单的注意力机制，用于确定更关注p6_up还是p5_in
+            p5_w1 = self.p5_w1_relu(self.p5_w1)
+            weight = p5_w1 / (torch.sum(p5_w1, dim=0) + self.epsilon)
+            p5_td = self.conv5_up(self.swish(weight[0] * p5_in_1 + weight[1] * self.p5_upsample(p6_td)))
+
+            # 简单的注意力机制，用于确定更关注p5_up还是p4_in
+            p4_w1 = self.p4_w1_relu(self.p4_w1)
+            weight = p4_w1 / (torch.sum(p4_w1, dim=0) + self.epsilon)
+            p4_td = self.conv4_up(self.swish(weight[0] * p4_in_1 + weight[1] * self.p4_upsample(p5_td)))
+
+            # 简单的注意力机制，用于确定更关注p4_up还是p3_in
+            p3_w1 = self.p3_w1_relu(self.p3_w1)
+            weight = p3_w1 / (torch.sum(p3_w1, dim=0) + self.epsilon)
+            p3_out = self.conv3_up(self.swish(weight[0] * p3_in + weight[1] * self.p3_upsample(p4_td)))
+
+            # 简单的注意力机制，用于确定更关注p4_in_2还是p4_up还是p3_out
+            p4_w2 = self.p4_w2_relu(self.p4_w2)
+            weight = p4_w2 / (torch.sum(p4_w2, dim=0) + self.epsilon)
+            # Connections for P4_0, P4_1 and P3_2 to P4_2 respectively
+            p4_out = self.conv4_down(
+                self.swish(weight[0] * p4_in_2 + weight[1] * p4_td + weight[2] * self.p4_downsample(p3_out)))
+
+            # 简单的注意力机制，用于确定更关注p5_in_2还是p5_up还是p4_out
+            p5_w2 = self.p5_w2_relu(self.p5_w2)
+            weight = p5_w2 / (torch.sum(p5_w2, dim=0) + self.epsilon)
+            p5_out = self.conv5_down(
+                self.swish(weight[0] * p5_in_2 + weight[1] * p5_td + weight[2] * self.p5_downsample(p4_out)))
+
+            # 简单的注意力机制，用于确定更关注p6_in还是p6_up还是p5_out
+            p6_w2 = self.p6_w2_relu(self.p6_w2)
+            weight = p6_w2 / (torch.sum(p6_w2, dim=0) + self.epsilon)
+            p6_out = self.conv6_down(
+                self.swish(weight[0] * p6_in + weight[1] * p6_td + weight[2] * self.p6_downsample(p5_out)))
+
+            # 简单的注意力机制，用于确定更关注p7_in还是p7_up还是p6_out
+            p7_w2 = self.p7_w2_relu(self.p7_w2)
+            weight = p7_w2 / (torch.sum(p7_w2, dim=0) + self.epsilon)
+            p7_out = self.conv7_down(self.swish(weight[0] * p7_in + weight[1] * self.p7_downsample(p6_out)))
+        else:
+            p3_in, p4_in, p5_in, p6_in, p7_in = inputs
+
+            # 简单的注意力机制，用于确定更关注p7_in还是p6_in
+            p6_w1 = self.p6_w1_relu(self.p6_w1)
+            weight = p6_w1 / (torch.sum(p6_w1, dim=0) + self.epsilon)
+            p6_td = self.conv6_up(self.swish(weight[0] * p6_in + weight[1] * self.p6_upsample(p7_in)))
+
+            # 简单的注意力机制，用于确定更关注p6_up还是p5_in
+            p5_w1 = self.p5_w1_relu(self.p5_w1)
+            weight = p5_w1 / (torch.sum(p5_w1, dim=0) + self.epsilon)
+            p5_td = self.conv5_up(self.swish(weight[0] * p5_in + weight[1] * self.p5_upsample(p6_td)))
+
+            # 简单的注意力机制，用于确定更关注p5_up还是p4_in
+            p4_w1 = self.p4_w1_relu(self.p4_w1)
+            weight = p4_w1 / (torch.sum(p4_w1, dim=0) + self.epsilon)
+            p4_td = self.conv4_up(self.swish(weight[0] * p4_in + weight[1] * self.p4_upsample(p5_td)))
+
+            # 简单的注意力机制，用于确定更关注p4_up还是p3_in
+            p3_w1 = self.p3_w1_relu(self.p3_w1)
+            weight = p3_w1 / (torch.sum(p3_w1, dim=0) + self.epsilon)
+            p3_out = self.conv3_up(self.swish(weight[0] * p3_in + weight[1] * self.p3_upsample(p4_td)))
+
+            # 简单的注意力机制，用于确定更关注p4_in还是p4_up还是p3_out
+            p4_w2 = self.p4_w2_relu(self.p4_w2)
+            weight = p4_w2 / (torch.sum(p4_w2, dim=0) + self.epsilon)
+            # Connections for P4_0, P4_1 and P3_2 to P4_2 respectively
+            p4_out = self.conv4_down(
+                self.swish(weight[0] * p4_in + weight[1] * p4_td + weight[2] * self.p4_downsample(p3_out)))
+
+            # 简单的注意力机制，用于确定更关注p5_in还是p5_up还是p4_out
+            p5_w2 = self.p5_w2_relu(self.p5_w2)
+            weight = p5_w2 / (torch.sum(p5_w2, dim=0) + self.epsilon)
+            p5_out = self.conv5_down(
+                self.swish(weight[0] * p5_in + weight[1] * p5_td + weight[2] * self.p5_downsample(p4_out)))
+
+            # 简单的注意力机制，用于确定更关注p6_in还是p6_up还是p5_out
+            p6_w2 = self.p6_w2_relu(self.p6_w2)
+            weight = p6_w2 / (torch.sum(p6_w2, dim=0) + self.epsilon)
+            p6_out = self.conv6_down(
+                self.swish(weight[0] * p6_in + weight[1] * p6_td + weight[2] * self.p6_downsample(p5_out)))
+
+            # 简单的注意力机制，用于确定更关注p7_in还是p7_up还是p6_out
+            p7_w2 = self.p7_w2_relu(self.p7_w2)
+            weight = p7_w2 / (torch.sum(p7_w2, dim=0) + self.epsilon)
+            p7_out = self.conv7_down(self.swish(weight[0] * p7_in + weight[1] * self.p7_downsample(p6_out)))
+
+        return p3_out, p4_out, p5_out, p6_out, p7_out
+
+    def _forward(self, inputs):
+        # 当phi=6、7的时候使用_forward
+        if self.first_time:
+            # 第一次BIFPN需要下采样与降通道获得
+            # p3_in p4_in p5_in p6_in p7_in
+            p3, p4, p5 = inputs
+            p3_in = self.p3_down_channel(p3)
+            p4_in_1 = self.p4_down_channel(p4)
+            p4_in_2 = self.p4_down_channel_2(p4)
+            p5_in_1 = self.p5_down_channel(p5)
+            p5_in_2 = self.p5_down_channel_2(p5)
+            p6_in = self.p5_to_p6(p5)
+            p7_in = self.p6_to_p7(p6_in)
+
+            p6_td = self.conv6_up(self.swish(p6_in + self.p6_upsample(p7_in)))
+
+            p5_td = self.conv5_up(self.swish(p5_in_1 + self.p5_upsample(p6_td)))
+
+            p4_td = self.conv4_up(self.swish(p4_in_1 + self.p4_upsample(p5_td)))
+
+            p3_out = self.conv3_up(self.swish(p3_in + self.p3_upsample(p4_td)))
+
+            p4_out = self.conv4_down(
+                self.swish(p4_in_2 + p4_td + self.p4_downsample(p3_out)))
+
+            p5_out = self.conv5_down(
+                self.swish(p5_in_2 + p5_td + self.p5_downsample(p4_out)))
+
+            p6_out = self.conv6_down(
+                self.swish(p6_in + p6_td + self.p6_downsample(p5_out)))
+
+            p7_out = self.conv7_down(self.swish(p7_in + self.p7_downsample(p6_out)))
+
+        else:
+            p3_in, p4_in, p5_in, p6_in, p7_in = inputs
+
+            p6_td = self.conv6_up(self.swish(p6_in + self.p6_upsample(p7_in)))
+
+            p5_td = self.conv5_up(self.swish(p5_in + self.p5_upsample(p6_td)))
+
+            p4_td = self.conv4_up(self.swish(p4_in + self.p4_upsample(p5_td)))
+
+            p3_out = self.conv3_up(self.swish(p3_in + self.p3_upsample(p4_td)))
+
+            p4_out = self.conv4_down(
+                self.swish(p4_in + p4_td + self.p4_downsample(p3_out)))
+
+            p5_out = self.conv5_down(
+                self.swish(p5_in + p5_td + self.p5_downsample(p4_out)))
+
+            p6_out = self.conv6_down(
+                self.swish(p6_in + p6_td + self.p6_downsample(p5_out)))
+
+            p7_out = self.conv7_down(self.swish(p7_in + self.p7_downsample(p6_out)))
+
+        return p3_out, p4_out, p5_out, p6_out, p7_out
+
 
 class PyramidFeatures(nn.Module):
     def __init__(self, C3_size, C4_size, C5_size, feature_size=256):
@@ -136,6 +447,7 @@ class ClassificationModel(nn.Module):
 
         return out2.contiguous().view(x.shape[0], -1, self.num_classes)
 
+
 class Resnet(nn.Module):
     def __init__(self, phi, load_weights=False):
         super(Resnet, self).__init__()
@@ -158,6 +470,7 @@ class Resnet(nn.Module):
 
         return [feat1,feat2,feat3]
 
+
 class Retinanet(nn.Module):
 
     def __init__(self, num_classes, phi, pretrain_weights=False):
@@ -171,18 +484,30 @@ class Retinanet(nn.Module):
             3: [512, 1024, 2048],
             4: [512, 1024, 2048],
         }[phi]
-
-        self.fpn = PyramidFeatures(fpn_sizes[0], fpn_sizes[1], fpn_sizes[2])
+        #
+        # self.fpn = PyramidFeatures(fpn_sizes[0], fpn_sizes[1], fpn_sizes[2])
         # self.regressionModel = RegressionModel(256)
         # self.classificationModel = ClassificationModel(256, num_classes=num_classes)
         # self.anchors = Anchors()
         # self._init_weights()
+        #
+        # BiFPN的重复次数
+        self.fpn_cell_repeats = [3, 3, 3, 3, 3, 3, 3, 3]
+        # BiFPN所用的通道数
+        self.fpn_num_filters = [256, 256, 256, 256, 256, 256, 256, 256]
+        self.num_channels = 256
+        self.bifpn = nn.Sequential(
+            *[BiFPN(self.fpn_num_filters[phi],
+                    fpn_sizes,
+                    True if _ == 0 else False,
+                    attention=True if phi < 5 else False)
+              for _ in range(self.fpn_cell_repeats[phi])])
         self.num_classes = num_classes
-        self.cls_head = FCOSClsCenterHead(256,
+        self.cls_head = FCOSClsCenterHead(self.num_channels,
                                           self.num_classes,
                                           num_layers=4,
                                           prior=0.01)
-        self.regcenter_head = FCOSRegHead(256, num_layers=4)
+        self.regcenter_head = FCOSRegHead(self.num_channels, num_layers=4)
 
         self.strides = torch.tensor([8, 16, 32, 64, 128], dtype=torch.float)
         self.positions = FCOSPositions(self.strides)
@@ -228,7 +553,11 @@ class Retinanet(nn.Module):
 
         p3, p4, p5 = self.backbone_net(inputs)
 
-        features = self.fpn([p3, p4, p5])
+        # features = self.fpn([p3, p4, p5])
+        features = (p3, p4, p5)
+        features = self.bifpn(features)
+
+        del p3, p4, p5
 
 
         self.fpn_feature_sizes = []
